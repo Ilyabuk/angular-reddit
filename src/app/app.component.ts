363ce47697b1f6570a6bd8d24fbbefd43aac7759
Букреев Илья Александрович <IlABukreev@vimpelcom.ru>
2018-11-09 08:14:04 +0300

    third commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,5 +5,11 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
+
 export class AppComponent {
+
+  addArticle(title: HTMLInputElement, link: HTMLInputElement): boolean {
+    console.log(`Adding article title: ${title.value} and link: ${link.value}`)
+    return false;
+  }
 }
